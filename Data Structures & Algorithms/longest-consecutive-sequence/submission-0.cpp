282c4b1778d9ec07ae9f9e79bfc9839e76008798
ahjsong <ahjsong@umich.edu>
2026-08-12 17:23:36 -0400

    Add: longest-consecutive-sequence - submission-0

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> myset;
+        for (const auto& val : nums) {
+            myset.insert(val);
+        }
+
+        int maxVal = 0;
+        unordered_set<int> seen;
+        for (const auto& val : myset) {
+            int counter = 0;
+            int current = val;
+            if (seen.count(current)) {
+                continue;
+            }
+
+            while (myset.count(current - 1)) {
+                current -= 1;
+            }
+
+            while (myset.count(current)) {
+                counter++;
+                current++;
+                seen.insert(current);
+            }
+            maxVal = max(maxVal, counter);
+        }
+
+        return maxVal;
+    }
+};
